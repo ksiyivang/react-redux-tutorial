6a045b4fcddb6645eeb570dbdf7072df03006303
khamla@siyivang <ksiyivang@gmail.com>
2021-05-09 22:16:38 +0700

    create Store subscribe dispatch redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
+
+import { createStore } from "redux"
+
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "ADD":
+      state += action.payload;
+      break;
+    case "SUBTRACT":
+      state -= action.payload;
+      break;
+    default:
+  }
+  return state;
+}
+
+
+const store = createStore(reducer, 15000);
+
+// subscribe == update state value
+store.subscribe(() => {
+  console.log("Update store: ", store.getState());
+})
+
+// manage any change action
+store.dispatch({
+  type: "ADD",
+  payload: 500
+});
+
+store.dispatch({
+  type: "SUBTRACT",
+  payload: 500
+});
