9e3744bf56d7562e5dbc27463928d3d352ede43b
khamla@siyivang <ksiyivang@gmail.com>
2021-05-10 15:46:19 +0700

    Fifth commit with react-component-redux-store-connect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React from 'react';
+import User from './pages/user';
+import { connect } from 'react-redux';
+
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <User username={props.user.name} />
+      <button onClick={() => props.setName("Redux Tutorial")}>Change Name</button>
     </div>
   );
 }
 
-export default App;
+const mapStatetoProps = (state) => {
+  return {
+    user: state.user,
+    employee: state.emp
+  };
+};
+
+const mapDispatchtoProps = (disatch) => {
+  return {
+    setName: (name) => {
+      disatch({
+        type: "setName",
+        payload: name
+
+      })
+    }
+  };
+}
+
+export default connect(mapStatetoProps, mapDispatchtoProps)(App);
