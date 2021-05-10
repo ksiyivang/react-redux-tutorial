48d8a4150a2e7dbb4764cb6656a8df355eb27438
khamla@siyivang <ksiyivang@gmail.com>
2021-05-10 14:42:02 +0700

    third commit with multiple-reducer

---
+++ src/module/3-multiple-reducer.js
@@ -0,0 +1,70 @@
+import { createStore, combineReducers } from "redux"
+const initialState = {
+    result: 15000,
+    value: [100]
+}
+
+// userRedcuer
+const userReducer = (state = { name: "khamla", age: 15 }, action) => {
+    switch (action.type) {
+        case "setName":
+            state = {
+                ...state,
+                name: action.payload
+            }
+            break;
+        case "setAge":
+            state = {
+                ...state,
+                age: action.payload
+            }
+            break;
+        default:
+    }
+    return state;
+}
+
+// employeeReducer
+const employeeReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case "ADD":
+            state = {
+                // value: state.value 
+                ...state,
+                result: state.result += action.payload,
+                value: [...state.value, action.payload]
+
+            }
+            break;
+        case "SUBTRACT":
+            state = {
+                // value: state.value 
+                ...state,
+                result: state.result += action.payload,
+                value: [...state.value, action.payload]
+
+            }
+            break;
+        default:
+    }
+    return state;
+}
+
+
+const store = createStore(combineReducers({ employeeReducer, userReducer }));
+
+// subscribe == update state value
+store.subscribe(() => {
+    console.log("Update store: ", store.getState());
+})
+
+// manage any change action
+store.dispatch({
+    type: "ADD",
+    payload: 25000
+});
+
+store.dispatch({
+    type: "setName",
+    payload: 200
+})
